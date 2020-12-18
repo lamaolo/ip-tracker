d3b42f42b26404f7d4287de0898eb52cd90d74a4
Lucero Amaolo <michilnx@gmail.com>
2020-12-17 22:54:08 -0300

    detalles y añadidos los creditos

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,7 +4,28 @@ import '../static/styles/components/Map.css';
 
 const Map = () => {
   // div que utiliza la librería para crear el mapa
-  return <div id="mapid"></div>;
+  return (
+    <>
+      <div id="mapid"></div>{' '}
+      <div className="credits">
+        <p>
+          Challenge by{' '}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0"
+          >
+            FrontendMentor
+          </a>
+          . Coded by{' '}
+          <a target="_blank" rel="noreferrer" href="https://github.com/lamaolo">
+            Lucero Amaolo
+          </a>
+          .
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Map;
